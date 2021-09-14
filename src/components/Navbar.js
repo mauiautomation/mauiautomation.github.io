e826609a3bf6f84311de412b36b7f6a60307c667
Thinh Tran <thinhtran@outlook.com.au>
2021-09-15 05:36:16 +0800

    adding Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+			
+		</div>
+
+        <div className="nav-links show-links"></div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
