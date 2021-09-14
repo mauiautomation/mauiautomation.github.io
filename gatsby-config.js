a89a3d4d645384b4dfa6dd4cec416af7adfa3dd9
Thinh Tran <thinhtran@outlook.com.au>
2021-09-15 05:45:33 +0800

    enable google analytich with streamID

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,12 +51,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `G-5HKW8M72JJ`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
